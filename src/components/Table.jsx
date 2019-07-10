ed46cabc6b3b375a1763ab473756f82b6c07e2fd
Ahmet Koylan <Ahmetburhan@gmail.com>
2019-07-10 08:47:25 -0700

    updates

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,4 +1,31 @@
 import React from "react";
+
+import BootstrapTable from "react-bootstrap-table-next";
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
+const { SearchBar } = Search;
+const columns = [
+  {
+    dataField: "templateId",
+    text: "Template ID"
+  },
+  {
+    dataField: "templateName",
+    text: "Template Name"
+  },
+  {
+    dataField: "title",
+    text: "Title"
+  },
+  {
+    dataField: "body",
+    text: "Body Message"
+  },
+  {
+    dataField: "status",
+    text: "Status"
+  }
+];
+
 export default class Table extends React.Component {
   constructor(props) {
     super(props);
@@ -8,7 +35,15 @@ export default class Table extends React.Component {
     console.log(data);
     return (
       <div>
-        <h1>Table Here</h1>
+        <ToolkitProvider keyField="id" data={data} columns={columns} search>
+          {props => (
+            <div>
+              <SearchBar {...props.searchProps} />
+              <hr />
+              <BootstrapTable {...props.baseProps} />
+            </div>
+          )}
+        </ToolkitProvider>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Table from "../src/components/Table";
 import SmsTable from "../src/components/smsTable";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./styles.css";
 
